e4e4afef5c2c50f6244147a7b452bdffabd3c3de
yiran.feng <fangyira@gmail.com>
2023-02-01 17:55:05 +0900

    feat: complete home page

--- Blog.Web/Pages/Index.cshtml.cs
+++ Blog.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Domain.Models;
+using Blog.Domain.Services.Client;
+using Blog.Web.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Blog.Web.Pages;
@@ -6,13 +9,44 @@ namespace Blog.Web.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly BlogService _blogService;
+    private readonly IDomainService _domainService;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public List<PostDto> Posts { get; set; } = new();
+    public List<DomainCategoryDto> Categories { get; set; } = new();
+    public List<DomainTopicDto> Topics { get; set; } = new();
+
+    public IndexModel(ILogger<IndexModel> logger, BlogService blogService, IDomainService domainService)
     {
         _logger = logger;
+        _blogService = blogService;
+        _domainService = domainService;
     }
 
-    public void OnGet()
+    public async Task OnGet()
     {
+        var domainInfo = await _domainService.GetInfo();
+
+        const int pageSize = 10;
+        var pageToken = string.Empty;
+        const int orderBy = 0;
+        const bool ascending = false;
+        var domainId = domainInfo.Id;
+        const bool publicOnly = true;
+
+        var result = await _blogService.ListPosts(
+            pageSize: pageSize,
+            pageToken: pageToken,
+            orderBy: orderBy,
+            ascending: ascending,
+            domainId: domainInfo.Id,
+            publicOnly: publicOnly,
+            categoryId: null,
+            topicId: null
+        );
+        Posts.AddRange(result.Data);
+
+        Categories = domainInfo.Categories.ToList();
+        Topics = domainInfo.Topics.ToList();
     }
 }
