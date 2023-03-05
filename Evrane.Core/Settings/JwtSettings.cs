522e3deafe96a2ede66ce8e97841dffa7a1834bd
Yiran <fangyira@gmail.com>
2023-03-05 17:53:06 +0900

    feat: update jwt settings

--- Evrane.Core/Settings/JwtSettings.cs
+++ Evrane.Core/Settings/JwtSettings.cs
@@ -3,6 +3,8 @@ namespace Evrane.Core.Settings;
 public class JwtSettings
 {
     public string Issuer { get; set; } = string.Empty;
+    
+    public List<string> Issuers { get; set; } = new List<string>();
     public string Audience { get; set; } = string.Empty;
     public string IssuerKeyPath { get; set; } = string.Empty;
 }
